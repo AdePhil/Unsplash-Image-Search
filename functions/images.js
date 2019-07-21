6c4c740c1f2b071cf387a9fc7db2bf0cd1f8fb68
= <=>
2019-07-21 03:20:42 +0100

    test deploy

--- functions/images.js
+++ functions/images.js
@@ -5,24 +5,21 @@ exports.handler = function(event, context, callback) {
   // const { query } = JSON.parse(event.body);
   const query = "dogs";
   const { APP_ID } = process.env;
-  callback(null, {
-    statusCode: 200,
-    body: "success"
-  });
 
-  // axios
-  //   .get(
-  //     `https://api.unsplash.com/search/photos?page=1&per_page=8&query=${query}&client_id=${APP_ID}`
-  //   )
-  //   .then(response => {
-  //     const body = response.data.results;
-  //     callback(null, {
-  //       statusCode: 200,
-  //       body: JSON.stringify(body)
-  //     });
-  //   })
-  //   .catch(error => {
-  //     console.log(error);
-  //     callback(new Error("Could not fetch data"));
-  //   });
+  axios
+    .get(
+      `https://api.unsplash.com/search/photos?page=1&per_page=8&query=${query}&client_id=${APP_ID}`
+    )
+    .then(response => {
+      const body = response.data.results;
+      const formattedBody = JSON.stringify(body) || [];
+      callback(null, {
+        statusCode: 200,
+        body: formattedBody
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      callback(new Error("Could not fetch data"));
+    });
 };
